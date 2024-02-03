8ff1d32a1da88e11486573b9ba22e318e118ceed
Vikram V B <vkrm1310@gmail.com>
2024-02-03 20:04:54 +0530

    Changed the tabspace indentation to 4 whitespace indentation

--- src/test/java/com/test/rest/example/TestRestApplicationTests.java
+++ src/test/java/com/test/rest/example/TestRestApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TestRestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
