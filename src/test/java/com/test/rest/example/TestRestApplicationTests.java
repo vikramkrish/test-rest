ba6434642f1b5593ad5dee0a7b182276b932065f
Vikram V B <vkrm1310@gmail.com>
2024-02-03 19:25:59 +0530

    Initial commit

---
+++ src/test/java/com/test/rest/example/TestRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.rest.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
